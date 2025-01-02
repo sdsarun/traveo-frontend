1a1c3fbb53699b4fc2fea491cefdee3943409330
Sarun Daunghirun <sdsarun@outlook.com>
2025-01-02 20:24:01 +0700

    fix(core): change types to partial

--- core/utils/common/yup.ts
+++ core/utils/common/yup.ts
@@ -1,17 +1,17 @@
 import type { ValidationError } from "yup";
 
-export type ValidationErrors<ObjectSchema = Record<string, any>> = Record<keyof ObjectSchema, string>;
+export type ValidationErrors<ObjectSchema = Record<string, any>> = Partial<Record<keyof ObjectSchema, string>>;
 
 export function parseYupValidationError<ObjectSchema extends Record<string, any>>(
   error: ValidationError
 ): ValidationErrors<ObjectSchema> {
-  const errors = {} as ValidationErrors<ObjectSchema>;
+  const errors: ValidationErrors<ObjectSchema> = {};
 
-  error.inner.forEach((err) => {
+  for (const err of error.inner) {
     if (err.path) {
       errors[err.path as keyof ObjectSchema] = err.message;
     }
-  });
+  }
 
   return errors;
 }
